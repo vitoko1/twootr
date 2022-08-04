505a90be5701fe0a6cf6e42dfaeda27da61c33f8
vitoko1 <38541062+vitoko1@users.noreply.github.com>
2022-08-03 21:55:48 -0700

    added header components

---
+++ twootr/frontend/src/components/ContainerHeader.style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const AppHeaderContainer = styled.div` 
+
+width: 100vw;
+height: 30vh;
+background-color: #413f42;
+
+`
+
+
+
---
+++ twootr/frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import {AppHeaderContainer} from './ContainerHeader.style';
+
+export default function Header() {
+  return (
+    <AppHeaderContainer>
+
+        <h1>Header</h1>
+
+
+    </AppHeaderContainer>
+    
+   
+  )
+}
+
