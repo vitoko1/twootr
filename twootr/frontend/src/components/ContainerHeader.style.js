a01264fe98bc66fec00b3c078366d7a067836ca1
vitoko1 <38541062+vitoko1@users.noreply.github.com>
2022-08-04 21:11:03 -0700

    adding generating random avatar

--- twootr/frontend/src/components/ContainerHeader.style.js
+++ twootr/frontend/src/components/ContainerHeader.style.js
@@ -7,7 +7,31 @@ height: 30vh;
 background-color: #413f42;
 text-align:center;
 
-`
+`;
 
+export const InputName = styled.input`
+  border: 0;
+  font-size: 16px;
+  border-bottom: 2px solid black;
+  font-family: monospace;
+  margin: 2px;
+`;
+
+export const InputLastName = styled.input`
+  border: 0;
+  font-size: 16px;
+  border-bottom: 2px solid black;
+  font-family: monospace;
+  margin: 2px;
+
+`;
+
+export const AvatarImage = styled.image `
+src: "https://avatars.dicebear.com/api/bottts/:seed.svg "
+
+`;
+
+
+//<img src="pic_trulli.jpg" alt="Italian Trulli"></img>
 
 
--- twootr/frontend/src/components/Header.jsx
+++ twootr/frontend/src/components/Header.jsx
@@ -1,16 +1,58 @@
 import React, { useState } from 'react';
-import {AppHeaderContainer} from './ContainerHeader.style';
+import { AppHeaderContainer, InputLastName, InputName, AvatarImage } from './ContainerHeader.style';
+
+
+
 
 export default function Header() {
-  return (
-    <AppHeaderContainer>
 
-        <h1>Twootr</h1>
+
+  const [imgURL, setImgURL] = React.useState("");
+  const [inputNameValue, setInputNameValue,] = React.useState("");
+  const [inputLastNameValue, setInputLastNameValue] = React.useState("");
+
+
+  const handleInputChangeName = (e) => {
+    setInputNameValue(() => {
+      if (e.target.value.length > 0) {
+        setImgURL(
+          `https://avatars.dicebear.com/api/bottts/${e.target.value}.svg`
+        );
+      }
+      return e.target.value;
+    });
+  };
+
+
+  const handleInputChangeLastName = (e) => {
+    setInputLastNameValue(() => {
+      if (e.target.value.length > 0) {
+        setImgURL(
+          `https://avatars.dicebear.com/api/bottts/${e.target.value}.svg`
+        );
+      }
+      return e.target.value;
+    });
+  };
+
+
+  return (
+
+
+    <AppHeaderContainer>
+      <img
+        src={imgURL}
+        width="70px"
+
+      ></img>
+      <h1>Twootr</h1>
+      < InputName className="form-control" onChange={handleInputChangeName} value={inputNameValue} placeholder="Name" />
+      < InputLastName className="form-control" onChange={handleInputChangeLastName} value={inputLastNameValue} placeholder="Last Name" />
 
 
     </AppHeaderContainer>
-    
-   
+
+
   )
 }
 
