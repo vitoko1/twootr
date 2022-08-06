ff5c95409d08484e43db3076ece96919b96f9af7
eacamposh <eacamposh@gmail.com>
2022-08-06 01:53:40 -0700

    add days posted

--- twootr/frontend/src/components/SectionViewTwoots/ViewTwoots.jsx
+++ twootr/frontend/src/components/SectionViewTwoots/ViewTwoots.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faFlag, faRotate } from "@fortawesome/free-solid-svg-icons";
@@ -14,25 +14,15 @@ import {
   DivDownIcon,
 } from "./ViewTwootsElement";
 
-/* function calculateDatetoDays(d1, d2) {
-  let aDate1 = d1.split("-");
-  let aDate2 = d2.split("-");
-  let fDate1 = Date.UTC(aDate1[2], aDate1[1] - 1, aDate1[0]);
-  let fDate2 = Date.UTC(aDate2[2], aDate2[1] - 1, aDate2[0]);
-  let dif = fDate2 - fDate1;
-  let days = Math.floor(dif / (1000 * 60 * 60 * 24));
-  
-
-  return days;
-}
-
-let d1 = new Date();
-console.log('current day', d1);
-let d2 = "2022-08-14";
-
-console.log("Days: ", calculateDatetoDays(d1, d2)); */
-
 export default function ViewTwoots() {
+  let today = new Date();
+
+  const thisYear = today.getFullYear();
+  const thisMonth = today.getMonth() + 1;
+  const thisDay = today.getDay()-1;
+
+  let dateFull = [thisYear + "-" + thisMonth + "-" + thisDay];
+
   const [twoots, setTwoowts] = useState([]);
 
   useEffect(() => {
@@ -41,6 +31,15 @@ export default function ViewTwoots() {
       .then((response) => setTwoowts(response.data))
       .catch((error) => console.log(error));
   }, []);
+
+  function dateToday(dayposted) {
+    let date1 = new Date(dateFull).getTime();
+    let date2 = new Date(dayposted).getTime();
+    let diff = date1 - date2;
+    let days = diff / (1000 * 60 * 60 * 24);
+
+    return days;
+  }
 
   return (
     <Container>
@@ -65,11 +64,11 @@ export default function ViewTwoots() {
               <p>{twoot.content}</p>
             </DivMid>
             <DivDown>
-              <p>{"Twooted " + twoot.dateAdded}</p>
-              <DivDownIcon> 
-                <FontAwesomeIcon id='flag' icon={faFlag}/>
-                <FontAwesomeIcon id='arrow' icon={faRotate} />
-                <FontAwesomeIcon id='heart' icon={faHeart} />
+              <p>{"Twooted " + dateToday(twoot.dateAdded) + " Days ago"}</p>
+              <DivDownIcon>
+                <FontAwesomeIcon id="flag" icon={faFlag} />
+                <FontAwesomeIcon id="arrow" icon={faRotate} />
+                <FontAwesomeIcon id="heart" icon={faHeart} />
               </DivDownIcon>
             </DivDown>
             <br />
