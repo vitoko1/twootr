e496cf6ebd356dcc00ea63ab8bc8c399bdd39387
eacamposh <eacamposh@gmail.com>
2022-08-05 23:09:14 -0700

    saved in server twoots data

--- twootr/frontend/src/components/SectionViewTwoots/ViewTwoots.jsx
+++ twootr/frontend/src/components/SectionViewTwoots/ViewTwoots.jsx
@@ -13,6 +13,24 @@ import {
   DivDownIcon,
 } from "./ViewTwootsElement";
 
+/* function calculateDatetoDays(d1, d2) {
+  let aDate1 = d1.split("-");
+  let aDate2 = d2.split("-");
+  let fDate1 = Date.UTC(aDate1[2], aDate1[1] - 1, aDate1[0]);
+  let fDate2 = Date.UTC(aDate2[2], aDate2[1] - 1, aDate2[0]);
+  let dif = fDate2 - fDate1;
+  let days = Math.floor(dif / (1000 * 60 * 60 * 24));
+  
+
+  return days;
+}
+
+let d1 = new Date();
+console.log('current day', d1);
+let d2 = "2022-08-14";
+
+console.log("Days: ", calculateDatetoDays(d1, d2)); */
+
 export default function ViewTwoots() {
   const [twoots, setTwoowts] = useState([]);
 
@@ -30,7 +48,11 @@ export default function ViewTwoots() {
           <div>
             <DivTop1>
               <img
-                src="https://avatars.dicebear.com/api/bottts/${authorSlug}.svg"
+                src={
+                  "https://avatars.dicebear.com/api/bottts/" +
+                  twoot.authorSlug +
+                  ".svg"
+                }
                 width="55px"
               ></img>
               <p>{twoot.author}</p>
@@ -42,13 +64,15 @@ export default function ViewTwoots() {
               <p>{twoot.content}</p>
             </DivMid>
             <DivDown>
-              <p>{"Twooted "+twoot.dateAdded}</p>
+              <p>{"Twooted " + twoot.dateAdded}</p>
               <DivDownIcon>
                 <FontAwesomeIcon icon={faFlag} style={{ color: "red" }} />
                 <FontAwesomeIcon icon={faRotate} />
                 <FontAwesomeIcon icon={faHeart} />
               </DivDownIcon>
             </DivDown>
+            <br />
+            <br />
           </div>
         ))}
       </ContainerTwoot>
--- twootr/server.js
+++ twootr/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import axios from 'axios';
-import { v4 as uuidv4 } from 'uuid';
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('docs'));
+app.use(express.static("docs"));
 app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
@@ -15,19 +15,22 @@ app.listen(port, () => {
 
 let twoots = [];
 
-app.get('/twoots', (req, res) => {
+app.get("/twoots", (req, res) => {
   axios
-    .get('https://quotable.io/quotes?page=1&maxLength=140')
+    .get("https://quotable.io/quotes?page=1&maxLength=140")
     .then((response) => {
       const newTwoots = [...twoots, ...response.data.results];
       res.status(200).json(newTwoots);
     });
 });
 
-app.post('/twoot', (req, res) => {
+app.post("/twoot", (req, res) => {
+  console.log("DATA!"+req.body.newTwoot);
   if (!req.body || !req.body.newTwoot) {
-    res.status(400).send('Invalid data! Please, review the documentation.');
+   
+    res.status(400).send("Invalid data! Please, review the documentation.");
   } else {
+    console.log("DATA:", req.body);
     const { author, authorSlug, content, dateAdded } = req.body.newTwoot;
     if (
       !author ||
@@ -42,7 +45,7 @@ app.post('/twoot', (req, res) => {
       res
         .status(400)
         .send(
-          'Invalid data! At least one of the fields is empty or missing. Please, review the data that was sent.'
+          "Invalid data! At least one of the fields is empty or missing. Please, review the data that was sent."
         );
     } else {
       const newTwoot = { ...req.body.newTwoot, _id: uuidv4() };
